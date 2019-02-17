57e3662f513ea8264e29c0662e02c15097d8e0a1
Vanessa Flores <flores2689@gmail.com>
2019-02-17 17:02:22 -0600

    Setup basic Firestore queries to read data into companiesTableView

--- JobSearchOrganizer/Controller/CompaniesViewController.swift
+++ JobSearchOrganizer/Controller/CompaniesViewController.swift
@@ -7,11 +7,28 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class CompaniesViewController: UIViewController {
     
     var addCompanyView: AddCompanyView!
 
+    
+    let firestore = Firestore.firestore()
+    private var listener: ListenerRegistration?
+    
+    private var companies: [Company] = []
+    private var documents: [DocumentSnapshot] = []
+    
+    fileprivate var query: Query? {
+        didSet {
+            if let listener = listener {
+                listener.remove()
+                observeQuery()
+            }
+        }
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -24,6 +41,7 @@ class CompaniesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationController?.navigationBar.isTranslucent = false
         navigationItem.title = "Companies"
         navigationController?.navigationBar.titleTextAttributes = [
             NSAttributedString.Key.font: UIFont(name: "Avenir", size: 28)!,
@@ -39,6 +57,8 @@ class CompaniesViewController: UIViewController {
         
         addCompanyView = AddCompanyView()
         addCompanyView.translatesAutoresizingMaskIntoConstraints = false
+        addCompanyView.delegate = self
+        addCompanyView.companyNameTextField.delegate = self
         
         setupView()
         
@@ -47,6 +67,62 @@ class CompaniesViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
     }
+    
+    // Mark: Firestore queries
+    
+    fileprivate func stopObserving() {
+        listener?.remove()
+    }
+    
+    fileprivate func baseQuery() -> Query {
+        return firestore.collection("companies")
+    }
+    
+    fileprivate func observeQuery() {
+        guard let query = query else { return }
+        stopObserving()
+        
+        // Display data from Firestore
+        listener = query.addSnapshotListener { [unowned self] (snapshot, error) in
+            guard let snapshot = snapshot else {
+                print("Error fetching snapshot results: \(error!)")
+                return
+            }
+            
+            let models = snapshot.documents.map { (document) -> Company in
+                if let model = Company(dictionary: document.data()) {
+                    return model
+                } else {
+                    // Don't use fatalError in real app here
+                    fatalError("Unable to initialize type \(Company.self) with dictionary \(document.data())")
+                }
+            }
+            self.companies = models
+            self.documents = snapshot.documents
+            
+            // TODO: call reloadData() on tableView
+        }
+    }
+    
+    // Mark: Write to Firestore
+    
+//    func addCompanyToCollection(company: Company) {
+//        let collection = firestore.collection("companies")
+//        
+//        let company = Company(
+//            name: company.name,
+//            hasContact: company.hasContact,
+//            motivation: company.motivation,
+//            currentlyHiring: company.currentlyHiring
+//        )
+//
+//        collection.addDocument(data: company.dictionary)
+//    }
+//
+    // Mark: Read from Firestore
+    
+    
+    // Mark: Set up the view
     
     func setupView() {
         view.addSubview(addCompanyView)
@@ -58,5 +134,46 @@ class CompaniesViewController: UIViewController {
         print(addCompanyView.frame.origin)
     }
 
+}
+
+// Mark: UITextFieldDelegate
+
+extension CompaniesViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let text = (textField.text! as NSString).replacingCharacters(in: range, with: string)
+        if textField == addCompanyView.companyNameTextField {
+            if !text.isEmpty {
+                addCompanyView.addCompanyButton.isEnabled = true
+            } else {
+                addCompanyView.addCompanyButton.isEnabled = false
+            }
+        }
+        
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField == addCompanyView.companyNameTextField {
+            guard let validatedText = textField.text?.trimmingCharacters(in: .whitespacesAndNewlines), !validatedText.isEmpty else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == addCompanyView.companyNameTextField {
+            textField.resignFirstResponder()
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == addCompanyView.companyNameTextField {
+            if textField.text != nil, textField.text != "" {
+                print(textField.text ?? "Textfield Empty")
+            } 
+        }
+        return true
+    }
 }
 
---
+++ JobSearchOrganizer/Model/Company.swift
@@ -0,0 +1,41 @@
+//
+//  Company.swift
+//  JobSearchOrganizer
+//
+//  Created by Vanessa Flores on 2/17/19.
+//  Copyright © 2019 Vanessa Flores. All rights reserved.
+//
+
+import Foundation
+
+struct Company {
+    
+    var name: String
+    var hasContact: Bool
+    var motivation: Int
+    var currentlyHiring: Int
+    
+    var dictionary: [String: Any] {
+        return [
+            "name": name,
+            "hasContact": hasContact,
+            "motivation": motivation,
+            "currentlyHiring": currentlyHiring,
+        ]
+    }
+    
+}
+
+extension Company: DocumentSerializable {
+    init?(dictionary: [String : Any]) {
+        guard let name = dictionary["name"] as? String,
+            let hasContact = dictionary["hasContact"] as? Bool,
+            let motivation = dictionary["motivation"] as? Int,
+            let currentlyHiring = dictionary["currentlyHiring"] as? Int else { return nil }
+        
+        self.init(name: name,
+                  hasContact: hasContact,
+                  motivation: motivation,
+                  currentlyHiring: currentlyHiring)
+    }
+}
